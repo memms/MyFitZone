c02608e26d4c2f6fdf4d3154fd51e9e408c63be6
Ansh Govil <devneraj3@gmail.com>
2023-06-18 20:15:42 -0400

    Added: BottomSheetDialog, Account Logout

--- app/src/main/java/com/example/myfitzone/Views/PreferencesFragment.kt
+++ app/src/main/java/com/example/myfitzone/Views/PreferencesFragment.kt
@@ -5,18 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.example.myfitzone.R
 
 
-class PreferencesFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preferences, container, false)
+class PreferencesFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        //TODO("Not yet implemented")
     }
 
 
--- app/src/main/java/com/example/myfitzone/Views/ProfileFragment.kt
+++ app/src/main/java/com/example/myfitzone/Views/ProfileFragment.kt
@@ -4,17 +4,65 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myfitzone.MainActivity
+import com.example.myfitzone.Models.AuthenticationModel
 import com.example.myfitzone.R
+import com.example.myfitzone.databinding.FragmentHomeBinding
+import com.example.myfitzone.databinding.FragmentProfileBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var authmodel: AuthenticationModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editButtonProfile.setOnClickListener {
+            showBottomSheetDialog()
+        }
+        authmodel = ViewModelProvider(requireActivity())[AuthenticationModel::class.java]
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetFragment: BottomSheetDialog = BottomSheetDialog(requireContext())
+        bottomSheetFragment.setContentView(R.layout.profile_dialog)
+        bottomSheetFragment.show()
+        bottomSheetFragment.findViewById<LinearLayout>(R.id.logout_row_profile_diag)?.setOnClickListener {
+            bottomSheetFragment.dismiss()
+            logout()
+        }
+        bottomSheetFragment.findViewById<LinearLayout>(R.id.first_row_profile_diag)?.setOnClickListener {
+            bottomSheetFragment.dismiss()
+            editProfile()
+        }
+    }
+
+    fun logout(){
+        authmodel.logout()
+    }
+
+    fun editProfile(){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
