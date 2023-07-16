e3619d85a4b37d8020e37792e98b909bc5e5511c
Ansh Govil <devneraj3@gmail.com>
2023-07-15 23:08:47 -0400

    Update to current

--- app/src/main/java/com/example/myfitzone/Views/NewExerciseFragment.kt
+++ app/src/main/java/com/example/myfitzone/Views/NewExerciseFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfitzone.databinding.FragmentNewExerciseBinding
 
 
@@ -24,12 +25,50 @@ class NewExerciseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.addAttributeNewExercise.setOnClickListener {
+            addAttribute()
+        }
+        binding.saveNewExercise.setOnClickListener {
+            saveExercise()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun addAttribute(){
+
+    }
+
+    private fun saveExercise(){
+
+    }
+
+    //RecyclerView Adapter that has a spinner inside it with a string array as the options. Store the selected option back into an arrayList in main class.
+    inner class NewExerciseAdapter : RecyclerView.Adapter<NewExerciseAdapter.ViewHolder>(){
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            TODO()
+        }
+
+        override fun getItemCount(): Int {
+            TODO()
+        }
+
+    }
+
+
+
 
 
 }
